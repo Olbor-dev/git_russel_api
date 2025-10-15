4bd830f4dd4767c0a5cb32bc41ccd14ca39d1d99
OlivierDev <olivierbordage@yahoo.fr>
2025-10-15 11:52:03 +0200

    Ajout de models/userModel.js

---
+++ models/userModel.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    nom: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('User', userSchema);
