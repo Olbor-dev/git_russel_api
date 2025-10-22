ca3ba68d8b85d552d73eb9cbe6430ad1d386c60c
Olbor-dev <olbor.dev@gmail.com>
2025-10-22 22:03:51 +0200

    fin de la mise en page de l'application, ajout de la documentation

--- routes/reservationRoutes.js
+++ routes/reservationRoutes.js
@@ -3,16 +3,9 @@ const router = express.Router();
 const reservationController = require('../controllers/reservationController');
 const isAuthenticated = require('../middlewares/isAuthenticaded');
 
-// Liste des réservations
 router.get('/', isAuthenticated, reservationController.getReservations);
-
-// Créer
 router.post('/create', isAuthenticated, reservationController.createReservation);
-
-// Modifier
 router.post('/update/:id', isAuthenticated, reservationController.updateReservation);
-
-// Supprimer
 router.get('/delete/:id', isAuthenticated, reservationController.deleteReservation);
 
 module.exports = router;
